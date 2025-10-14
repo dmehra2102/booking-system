5acd170ab0be89af9648b1325dafa650a424bdfd
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-10-14 19:43:03 +0530

    feat: added validation helper functions

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.1
 
 require (
 	github.com/gin-gonic/gin v1.11.0
+	github.com/go-playground/validator/v10 v10.27.0
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/joho/godotenv v1.5.1
 	github.com/prometheus/client_golang v1.23.2
@@ -31,7 +32,6 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
---
+++ pkg/validation/validation.go
@@ -0,0 +1,60 @@
+package validation
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validate *validator.Validate
+
+func init(){
+	validate = validator.New()
+
+	// Register custom validators
+	validate.RegisterValidation("password", validatePassword)
+
+	validate.RegisterTagNameFunc(func(field reflect.StructField) string {
+		name := strings.SplitN(field.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+}
+
+func ValidateStruct(s any) error {
+	return validate.Struct(s)
+}
+
+func validatePassword(f1 validator.FieldLevel) bool {
+	password := f1.Field().String()
+	return len(password) >= 8
+}
+
+func GetValidationErrors(err error) map[string]string {
+	errors := make(map[string]string)
+	
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		for _, e := range validationErrors {
+			field := e.Field()
+			switch e.Tag() {
+			case "required":
+				errors[field] = field + " is required"
+			case "email":
+				errors[field] = field + " must be a valid email"
+			case "min":
+				errors[field] = field + " must be at least " + e.Param() + " characters"
+			case "max":
+				errors[field] = field + " must be at most " + e.Param() + " characters"
+			case "password":
+				errors[field] = field + " must be at least 8 characters long"
+			default:
+				errors[field] = field + " is invalid"
+			}
+		}
+	}
+	
+	return errors
+}
